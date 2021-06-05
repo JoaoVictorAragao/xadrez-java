adb43b666d89b3b722ab4e641e1f2273bac248c6
JoaoVictorAragao <jojoba2018cnx@gmail.com>
2021-06-05 14:42:26 -0300

    Limpar tela e Exceções melhoradas

--- src/application/main.java
+++ src/application/main.java
@@ -1,7 +1,9 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import Xadrex.ExcessaoDXadrez;
 import Xadrex.Partida;
 import Xadrex.PosicaoXadrez;
 import Xadrex.XadrexPeça;
@@ -11,21 +13,29 @@ public class main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		Tabuleiro t1 = new Tabuleiro(8, 8);
+		//Tabuleiro t1 = new Tabuleiro(8, 8);
 		Partida p1 = new Partida();
-		
+
 		while (true) {
-			UI.printBoard(p1.getPecas());
-			System.out.println();
-			System.out.println("Origem: ");
-			PosicaoXadrez origem = UI.LerPosicao(sc);
-			
-			System.out.println();
-			System.out.println("Destino: ");
-			PosicaoXadrez destino = UI.LerPosicao(sc);
-			
-			XadrexPeça refem = p1.Movimento(origem, destino); 
+			try {
+				UI.LimpaTela();
+				UI.printBoard(p1.getPecas());
+				System.out.println();
+				System.out.println("Origem: ");
+				PosicaoXadrez origem = UI.LerPosicao(sc);
+
+				System.out.println();
+				System.out.println("Destino: ");
+				PosicaoXadrez destino = UI.LerPosicao(sc);
+
+				XadrexPeça refem = p1.Movimento(origem, destino);
+			} catch (ExcessaoDXadrez e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			} catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 		}
 	}
-
 }
